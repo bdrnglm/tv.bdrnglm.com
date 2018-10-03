dfdd99b0a4aa79b14f8968d2f2293340ff5c2a43
BDRN <bdrnglm@gmail.com>
2018-10-03 19:16:29 +0200

    Redirection to main page with auto subscription if no video specified

--- scripts/YouTube_redirect.js
+++ scripts/YouTube_redirect.js
@@ -1,13 +1,13 @@
-var index, v;
+var index, href, v;
 var url = new URL(window.location.href);
 var day = url.searchParams.get("day");
 
 if (day != null && day > 0 && day <= days.length) {
 	v = days[day - 1];
   index = day;
+	href = `https://www.youtube.com/watch?v=${v}&index=${index}&list=${playlist}`;
 } else {
-	v = days[days.length - 1];
-  index = days.length;
+	href = `https://www.youtube.com/c/bdrnglm?sub_confirmation=1`;
 }
 
-window.location.href = (`https://www.youtube.com/watch?v=${v}&index=${index}&list=${playlist}`);
+window.location.href = href;

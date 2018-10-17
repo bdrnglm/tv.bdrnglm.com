2838597bc0683994da6458695c066791879174dd
BDRN <bdrnglm@gmail.com>
2018-10-17 08:23:59 +0200

    Fixed URL redirection to atually redirect to the list's main page + miscellaneous code refactoring

--- scripts/YouTube_redirect.js
+++ scripts/YouTube_redirect.js
@@ -1,16 +1,28 @@
+// Creating and initialising needed variables: Actual URL + eventual argument
 var index, href, v;
 var url = new URL(window.location.href);
 var day = url.searchParams.get("day");
 
+href = `https://www.youtube.com/`;
+
+// Check if argument "day" is present
 if (day != null) {
-	href = `https://www.youtube.com/watch?list=${playlist}`;
+	// If so check if it ranges from 0 to the biggest video number
 	if (day > 0 && day <= days.length) {
+		// If so appends to the new URL the ID and list's index of the video
 		v = days[day - 1];
 	  index = day;
-		href += `&v=${v}&index=${index}`;
+		href += `watch?v=${v}&index=${index}&`;
+	} else {
+		// Otherwise it just append the "playlist" keyword to the new URL to redirect to the playlist's main page
+		href += `playlist?`;
 	}
+	// Since "day" was present in the original URL append playlist's ID
+	href += `list=${playlist}`;
 } else {
-	href = `https://www.youtube.com/c/bdrnglm?sub_confirmation=1`;
+	// Default behaviour
+	href += `c/bdrnglm?sub_confirmation=1`;
 }
 
+// Redirect to the new URL
 window.location.href = href;

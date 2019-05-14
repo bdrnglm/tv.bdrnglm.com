c56bc5c38d105fe1a2ec56fad6fd9e3a79c3811e
BDRN <bdrnglm@gmail.com>
2019-05-14 19:51:04 +0200

    Fixed Link Redirect

--- scripts/YouTube_redirect.js
+++ scripts/YouTube_redirect.js
@@ -7,8 +7,6 @@ href = `https://www.youtube.com/`;
 
 // Check if argument "day" is present
 if (day != null) {
-	// Since "day" was present in the original URL append playlist's ID
-	href += `list=${playlist}`;
 	// If so check if it ranges from 0 to the biggest video number
 	if (day > 0 && day <= days.length) {
 		// If so appends to the new URL the ID and playlist's index of the video
@@ -19,6 +17,8 @@ if (day != null) {
 		// Otherwise it just append the "playlist" keyword to the new URL to redirect to the playlist's main page
 		href += `playlist?`;
 	}
+	// Since "day" was present in the original URL append playlist's ID
+	href += `list=${playlist}`;
 } else {
 	// Default behaviour
 	href += `c/bdrnglm?sub_confirmation=1`;
